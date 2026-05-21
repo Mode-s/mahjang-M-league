83e11539e589d9d7101fe4527d7d8bdcd6ccc2d7
Mode-s <shota.umemoto7@gmail.com>
2026-05-21 17:58:38 +0900

    first commit

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  siteName: '麻雀スタジオMリーチ',
+  lang: 'ja',
+  locale: 'ja_JP',
+  url: 'https://example.com',
+  defaultOgImage: '/ogp.jpg',
+
+  defaultTitle: '麻雀スタジオMリーチ',
+  titleTemplate: '%s | 店舗名',
+  description: 'サイト全体の説明文',
+
+  twitter: '',
+
+  googleFonts: 'M+PLUS+1p',
+}
