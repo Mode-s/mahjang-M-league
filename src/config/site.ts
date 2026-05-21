b36676f1db493dd701265228148065b2fc6d0bbf
Mode-s <shota.umemoto7@gmail.com>
2026-05-21 18:37:37 +0900

    add meta

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,12 +2,12 @@ export const siteConfig = {
   siteName: '麻雀スタジオMリーチ',
   lang: 'ja',
   locale: 'ja_JP',
-  url: 'https://example.com',
+  url: 'https://mahjang-m-league.pages.dev/',
   defaultOgImage: '/ogp.jpg',
 
   defaultTitle: '麻雀スタジオMリーチ',
-  titleTemplate: '%s | 店舗名',
-  description: 'サイト全体の説明文',
+  titleTemplate: '%s | 麻雀スタジオMリーチ',
+  description: '福井市大宮の麻雀スタジオMリーチ。初心者の方も安心、明るく清潔感のある店内でフリー・セット利用ともにお楽しみいただけます。',
 
   twitter: '',
 
